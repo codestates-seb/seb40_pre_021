33f4e8043a895b61a37d56f4289c4289539c02a7
mozzi327 <mozzi327@gmail.com>
2022-10-25 17:50:34 +0900

    INIT: backend 초기 프로젝트 셋팅

---
+++ pre-project/src/main/java/com/pre21/PreProjectApplication.java
@@ -0,0 +1,13 @@
+package com.pre21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PreProjectApplication.class, args);
+    }
+
+}
