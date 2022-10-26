264fcd4a730b01945d98103a69af2a188e674674
dev32user <dev32user@gmail.com>
2022-10-26 09:23:56 +0900

    Feat: User entity update
    초안 작성

--- backend/src/main/java/com/pre21/entity/User.java
+++ backend/src/main/java/com/pre21/entity/User.java
@@ -1,4 +1,33 @@
 package com.pre21.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "Users")
 public class User {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long user_id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column(nullable = false)
+    private LocalDateTime created_at = LocalDateTime.now();
+
+    @Column(nullable = false, name = "LAST_MODIFIED_AT")
+    private LocalDateTime modified_at = LocalDateTime.now();
 }
