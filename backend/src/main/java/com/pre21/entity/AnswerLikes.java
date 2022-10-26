c2f3446d5cc5e38adf13463e40b5bb9ded0817d7
Cristiano-Cho <nedved112233@gmail.com>
2022-10-26 23:06:38 +0900

    FEAT : 답변추천, 질문추천, tags Entitiy 추가

--- backend/src/main/java/com/pre21/entity/AnswerLikes.java
+++ backend/src/main/java/com/pre21/entity/AnswerLikes.java
@@ -1,4 +1,26 @@
 package com.pre21.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
 public class AnswerLikes {
+    @Column(nullable = false)
+    private Long question_id;
+
+    @Column(nullable = false)
+    private Long user_id;
+
+    @Column(nullable = false)
+    private boolean like_Yn = false;
+
+    @Column(nullable = false)
+    private boolean unlike_Yn = false;
 }
