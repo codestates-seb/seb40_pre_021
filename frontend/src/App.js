17c83e8ac73b0fe49aed0156ea33db16c56424c3
moon <moonjh9392@gmail.com>
2022-10-26 16:33:51 +0900

    header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,11 @@
+import Header from './components/Header/Header';
+
 function App() {
-	return <div>스택오버플로우</div>;
+	return (
+		<div>
+			<Header />
+		</div>
+	);
 }
 
 export default App;
---
+++ frontend/src/components/common/Button.jsx
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+
+const ButtonStyle = styled.button`
+	position: relative;
+	display: inline-block;
+	padding: 0.8em;
+	border: 1px solid ${(props) => props.borderColor || 'transparent'};
+	border-radius: 3px;
+	font-family: inherit;
+	font-size: 13px;
+	font-weight: bold;
+	line-height: calc(15 / 13);
+	text-align: center;
+	text-decoration: none;
+	cursor: pointer;
+	user-select: none;
+	box-shadow: inset 0 1px 0 0
+		hsl(0deg 0% 100% / ${(props) => props.shadowPersent || '40%'});
+	background-color: ${(props) => props.background || 'hsl(206,100%,52%)'};
+	color: ${(props) => props.color || 'hsl(0,0%,100%);'};
+	height: ${(props) => props.height || ''};
+
+	&:hover {
+		filter: brightness(90%);
+	}
+`;
+//기본은 Ask Question (푸른색)색깔
+//버튼 색상 변경 필요 시 props 로 borderColor,background,color,shadowPersent 받아서 필요한것만 바꾸면됨
+const Button = ({ text, callback, ...props }) => {
+	return (
+		<ButtonStyle {...props} onClick={callback}>
+			{text}
+		</ButtonStyle>
+	);
+};
+
+export default Button;
