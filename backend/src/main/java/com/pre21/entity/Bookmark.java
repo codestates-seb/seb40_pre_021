5589e554462faa24d7eb1a09269554874f849f75
dev32user <dev32user@gmail.com>
2022-10-26 09:31:11 +0900

    Feat: Bookmark entity update
    Bookmark entity 초안 작성

--- backend/src/main/java/com/pre21/entity/Bookmark.java
+++ backend/src/main/java/com/pre21/entity/Bookmark.java
@@ -1,4 +1,26 @@
 package com.pre21.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table
 public class Bookmark {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bookmark_id;
+
+    @Column
+    private Long user_id;
+
+    @Column
+    private Long question_id;
+
+    @Column
+    private Long answer_id;
 }
