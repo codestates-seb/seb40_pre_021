0abb307b2dc7761da2f23dff55dafff2f69a07d4
jooyong-boo <qnwndyd159@naver.com>
2022-10-27 12:43:06 +0900

    Feat: 마이페이지 aside 컴포넌트

---
+++ frontend/src/components/Mypage/Activity/SummaryAside.jsx
@@ -0,0 +1,50 @@
+import styled from 'styled-components';
+
+const SummaryAside = ({ img, size, title, content, footer, basis }) => {
+	return (
+		<Aside basis={basis}>
+			<ContentBox>
+				<img src={img} alt="" width={size} height={size} />
+				<AsideTitle>{title}</AsideTitle>
+				<AsideContent>{content}</AsideContent>
+				{footer}
+			</ContentBox>
+		</Aside>
+	);
+};
+
+export default SummaryAside;
+
+const Aside = styled.aside`
+	flex-basis: ${(props) => props.basis && `calc(${props.basis}% - 16px)`};
+	flex-grow: 1;
+	margin: 8px 8px 8px 0px;
+	padding: 12px;
+	border-radius: 5px;
+	border: 1px solid #d7d9dc;
+`;
+
+const ContentBox = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	text-align: center;
+	padding: 24px 0;
+	gap: 8px;
+`;
+
+const AsideTitle = styled.h2`
+	font-size: 1.1rem;
+	margin-bottom: 2px;
+	flex: 0 auto;
+	font-weight: 500;
+`;
+
+const AsideContent = styled.p`
+	line-height: 20px;
+	color: #6a737c;
+	font-size: 0.9rem;
+	padding: 0 8px;
+	margin-bottom: 12px;
+	font-weight: 500;
+`;
