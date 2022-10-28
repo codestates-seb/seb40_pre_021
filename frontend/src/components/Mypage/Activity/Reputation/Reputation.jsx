331de80d87d2016d161574fd3d1c0579376f9a51
jooyong-boo <qnwndyd159@naver.com>
2022-10-28 12:03:52 +0900

    Feat: Reputation 컴포넌트

---
+++ frontend/src/components/Mypage/Activity/Reputation/Reputation.jsx
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+import ListBox from '../ListBox';
+import Title from '../Title';
+
+const Reputation = () => {
+	return (
+		<Container>
+			<TitleBox>
+				<Title title="Reputation" />
+			</TitleBox>
+			<ListBox text="You have no recent reputation changes." />
+		</Container>
+	);
+};
+
+export default Reputation;
+
+const Container = styled.div`
+	&&& {
+		height: 100%;
+		display: flex;
+		flex-direction: column;
+	}
+`;
+
+const TitleBox = styled.div`
+	display: flex;
+	margin-bottom: 8px;
+	align-items: flex-end;
+	justify-content: space-between;
+`;
