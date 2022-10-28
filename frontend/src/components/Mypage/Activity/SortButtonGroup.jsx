ff84fdaeb0fec8253b1e3271c157c78c62190b07
jooyong-boo <qnwndyd159@naver.com>
2022-10-28 12:04:17 +0900

    Feat: 리스트 정렬버튼 컴포넌트

---
+++ frontend/src/components/Mypage/Activity/SortButtonGroup.jsx
@@ -0,0 +1,63 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+
+const SortButtonGroup = ({ data }) => {
+	const [menu, setMenu] = useState(data);
+
+	const handleMenuChange = (id) => {
+		let newMenu = menu.map((item) =>
+			item.id === id ? { ...item, clicked: true } : { ...item, clicked: false },
+		);
+		setMenu(newMenu);
+	};
+
+	return (
+		<GroupContainer>
+			{menu.map((item) => {
+				const { id, name, clicked } = item;
+				return (
+					<Menu
+						key={id}
+						clicked={clicked}
+						onClick={() => {
+							handleMenuChange(id);
+						}}>
+						{name}
+					</Menu>
+				);
+			})}
+		</GroupContainer>
+	);
+};
+
+export default SortButtonGroup;
+
+const GroupContainer = styled.div`
+	display: flex;
+	flex-wrap: wrap;
+	margin-bottom: 1px;
+	border-radius: 3px;
+`;
+
+const Menu = styled.a`
+	padding: 0.5rem;
+	font-size: 11px;
+	font-weight: 600;
+	box-shadow: none;
+	border: 1px solid #838c95;
+	margin-right: -1px;
+	border-radius: 0;
+	background-color: ${(props) => (props.clicked ? '#e3e6e8' : 'white')};
+	color: ${(props) => (props.clicked ? '#3b4045' : '#6a737c')};
+	:hover {
+		background-color: ${(props) => (props.clicked ? '#e3e6e8' : '#f8f9f9')};
+	}
+	&:first-child {
+		border-top-left-radius: 3px;
+		border-bottom-left-radius: 3px;
+	}
+	&:last-child {
+		border-top-right-radius: 3px;
+		border-bottom-right-radius: 3px;
+	}
+`;
