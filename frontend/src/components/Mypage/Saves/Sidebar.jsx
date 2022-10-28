289d9661bed1d7e875591bf3317dc6bda00d0559
jooyong-boo <qnwndyd159@naver.com>
2022-10-29 00:35:10 +0900

    Feat: 북마크 컴포넌트

---
+++ frontend/src/components/Mypage/Saves/Sidebar.jsx
@@ -0,0 +1,84 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+
+let data = [
+	{
+		id: 0,
+		name: 'All saves',
+		clicked: true,
+	},
+	{
+		id: 1,
+		name: 'For later',
+		clicked: false,
+	},
+];
+
+const Sidebar = () => {
+	const [tabs, setTabs] = useState(data);
+
+	const handleTabChange = (id) => {
+		let newTabs = tabs.map((tab) =>
+			tab.id === id ? { ...tab, clicked: true } : { ...tab, clicked: false },
+		);
+		setTabs(newTabs);
+	};
+	return (
+		<Container>
+			<ListBox>
+				<List>
+					{tabs.map((item) => {
+						const { id, name, clicked } = item;
+						return (
+							<ListText
+								key={id}
+								clicked={clicked}
+								onClick={() => {
+									handleTabChange(id);
+								}}>
+								{name}
+							</ListText>
+						);
+					})}
+				</List>
+			</ListBox>
+		</Container>
+	);
+};
+
+export default Sidebar;
+
+const Container = styled.div`
+	width: 210px;
+	margin-right: 32px;
+	flex: none;
+`;
+
+const ListBox = styled.ul`
+	display: flex;
+	flex-direction: column;
+	flex-wrap: wrap;
+	list-style: none;
+`;
+
+const List = styled.li`
+	list-style: none;
+`;
+
+const ListText = styled.span`
+	white-space: normal;
+	background-color: ${(props) => (props.clicked ? '#f48225' : 'white')};
+	color: ${(props) => (props.clicked ? 'white' : '#525960')};
+	display: flex;
+	align-items: center;
+	padding: 8px 12px;
+	border: none;
+	box-shadow: none;
+	cursor: pointer;
+	border-radius: 1000px;
+	font-weight: 600;
+	font-size: 14px;
+	:hover {
+		background-color: ${(props) => (props.clicked ? '#DA680C' : '#e4e6e8')};
+	}
+`;
