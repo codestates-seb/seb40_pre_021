cae12570db26658dd4811bcbc43cd7913204737a
jooyong-boo <qnwndyd159@naver.com>
2022-11-01 00:27:30 +0900

    Feat: 마이페이지 데이터 조회 hook 추가

---
+++ frontend/src/hooks/useMypageData.js
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react';
+import {
+	getMypageUserAnswer,
+	getMypageUserBookmark,
+	getMypageUserQuestion,
+	getMypageUserTag,
+} from '../api/mypageApi';
+
+function useMypageData(category, value = []) {
+	const [data, setData] = useState({ [category]: value });
+
+	useEffect(() => {
+		switch (category) {
+			case 'answer':
+				getMypageUserAnswer().then((res) => {
+					setData({ ...data, [category]: res });
+				});
+				break;
+			case 'question':
+				getMypageUserQuestion().then((res) => {
+					setData({ ...data, [category]: res });
+				});
+				break;
+			case 'tag':
+				getMypageUserTag().then((res) => {
+					setData({ ...data, [category]: res });
+				});
+				break;
+			case 'bookmark':
+				getMypageUserBookmark().then((res) => {
+					setData({ ...data, [category]: res });
+				});
+				break;
+		}
+	}, [category]);
+
+	return [data[category], setData];
+}
+
+export default useMypageData;
