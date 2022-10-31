263b0527af7419544c76cd3fd66d970965dad1fb
Seungyoon Yu <seung.yoon.yu@gmail.com>
2022-10-31 15:21:25 +0900

    FEAT: 에디터 css가 글자를 입력하면 미리보기에 바로 적용되도록 수정

--- frontend/src/components/Question/Editor.jsx
+++ frontend/src/components/Question/Editor.jsx
@@ -5,21 +5,54 @@ import textCharacterCheck from '../../utils/textCharacterCheck';
 const Container = styled.div``;
 const Header = styled.div``;
 const Textarea = styled.textarea``;
-const Result = styled.div``;
+const Result = styled.div`
+	ul {
+	}
+	ol {
+	}
+
+	h1 {
+		font-weight: 700;
+	}
+	h2 {
+	}
+	h3 {
+	}
+
+	strong {
+		font-weight: 700;
+	}
+	em {
+	}
+	hr {
+	}
+
+	pre {
+	}
+	code {
+	}
+
+	p {
+	}
+`;
 const Editor = () => {
 	const [mdText, setMdText] = useState('');
 	const handleChange = (e) => {
 		setMdText(parsedHTML(e.target.value));
 	};
+
 	return (
 		<>
 			<Container>
 				<Header />
 				<Textarea onChange={handleChange}></Textarea>
-				<Result mdText={mdText}>{mdText}</Result>
+				<Result
+					mdText={mdText}
+					dangerouslySetInnerHTML={{ __html: mdText }}></Result>
 			</Container>
 		</>
-	);
+	); // XSS공격 대비 필요. 마크다운 텍스트 에디터를 웹으로 만드는 것의 한계인 듯하다.
+	// https://ui.toast.com/weekly-pick/ko_monthly_202006 참고. 여기서도 수동으로 XSS의 가능성이 있는 코드들을 분석해서 따로 파싱한다.
 };
 
 export default Editor;
