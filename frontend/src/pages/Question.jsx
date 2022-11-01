936dc7e28fb09df9f3b95cfda8d6576bb927ca70
Seungyoon Yu <seung.yoon.yu@gmail.com>
2022-11-01 19:39:41 +0900

    FIX: Editor 컴포넌트 common 폴더로 이동, 그에 따른 import 수정

--- frontend/src/pages/Question.jsx
+++ frontend/src/pages/Question.jsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 import { useState } from 'react';
-import Editor from '../components/Question/Editor';
+import Editor from '../components/common/Editor';
 
 const QuestionContainer = styled.section``;
 const QuestionGroup = styled.article``;
@@ -21,30 +21,128 @@ const EditGroup = styled.div``;
 const YourAnswer = styled.h1``;
 const PostYourAnswer = styled.button``;
 
+const QuestionEx = {
+	question: [
+		{
+			questionId: 'string',
+			title: 'string',
+			body: 'string',
+			tags: 'string',
+			votes: 'int',
+			answers: 'int',
+			views: 'int',
+			nickname: 'string',
+			createdAt: 'string',
+			comments: [
+				{
+					commentId: 'string',
+					commentBody: 'string2',
+					createdAt: 'string',
+					nickname: 'string',
+				},
+				{
+					commentId: 'string',
+					commentBody: 'string2',
+					createdAt: 'string',
+					nickname: 'string',
+				},
+			],
+		},
+	],
+};
+const AnswerEx = {
+	answer: [
+		{
+			answers: [
+				{
+					answerId: 'string',
+					body: 'string',
+					votes: 'int',
+					bookmarked: 'bool',
+					choosed: 'bool',
+					createdAt: 'string',
+					modifiedAt: 'string',
+					comments: [
+						{
+							commentId: 'string',
+							commentBody: 'string',
+							createdAt: 'string',
+							nickname: 'string',
+						},
+						{
+							commentId: 'string1',
+							commentBody: 'string2',
+							createdAt: 'string3',
+							nickname: 'string4',
+						},
+					],
+				},
+				{
+					answerId: 'string2',
+					body: 'string4',
+					votes: 'int',
+					bookmarked: 'bool',
+					choosed: 'bool',
+					createdAt: 'string',
+					modifiedAt: 'string',
+					comments: [
+						{
+							commentId: 'string',
+							commentBody: 'string',
+							createdAt: 'string',
+							nickname: 'string',
+						},
+						{
+							commentId: 'string1',
+							commentBody: 'string2',
+							createdAt: 'string3',
+							nickname: 'string4',
+						},
+					],
+				},
+			],
+		},
+	],
+};
+
 const Question = () => {
 	return (
 		<>
 			<QuestionContainer>
 				<QuestionGroup>
 					<Header>
-						<Title />
-						<Info />
+						<Title>{QuestionEx.question[0].title}</Title>
+						<Info>Asked {QuestionEx.question[0].createdAt}</Info>
 						<AskQuestion />
 					</Header>
 					<QController />
-					<Body />
-					<Tags />
-					<Options />
-					<Profile />
-					<Comments />
+					<Body>{QuestionEx.question[0].body}</Body>
+					<Tags>{QuestionEx.question[0].tags}</Tags>
+					<Options>
+						<span>Share</span>
+						<span>Edit</span>
+					</Options>
+					<Profile></Profile>
+					{QuestionEx.question[0].comments.map((c) => (
+						<Comments key={c.commentId}>{c.commentBody}</Comments>
+					))}
 				</QuestionGroup>
 				<AnswerGroup>
-					<AnswerCount />
-					<AController />
-					<Body />
-					<Options />
-					<Profile />
-					<Comments />
+					<AnswerCount>{AnswerEx.answer[0].answers.length} Answers</AnswerCount>
+					{AnswerEx.answer[0].answers.map((el) => (
+						<>
+							<AController />
+							<Body>{el.body}</Body>
+							<Options>
+								<span>Share</span>
+								<span>Edit</span>
+							</Options>
+							<Profile></Profile>
+							{el.comments.map((c) => (
+								<Comments key={c.commentId}>{c.commentBody}</Comments>
+							))}
+						</>
+					))}
 				</AnswerGroup>
 				<EditGroup>
 					<YourAnswer />
