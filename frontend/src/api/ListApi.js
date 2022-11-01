a09a5a72563d89f58355f4f2a8bbf8570feade6c
moon <moonjh9392@gmail.com>
2022-11-01 15:22:21 +0900

    Feat: pagination 기능 구현

--- frontend/src/api/ListApi.js
+++ frontend/src/api/ListApi.js
@@ -6,10 +6,13 @@ const axiosConfig = {
 
 const instance = axios.create(axiosConfig);
 
-export const getList = async () => {
+export const getList = async (data) => {
 	try {
-		const result = await instance.get(`/questions
-		`);
+		const result = await instance.get(
+			`/questions
+		`,
+			data,
+		);
 		return result.data;
 	} catch (err) {
 		console.log(err);
