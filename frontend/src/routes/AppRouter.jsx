20402d72f20686ded41090a582bc562b35c9b126
jooyong-boo <qnwndyd159@naver.com>
2022-11-01 14:26:48 +0900

    Feat: 존재하지 않는 url 접속시 메인페이지로 리다이렉트

--- frontend/src/routes/AppRouter.jsx
+++ frontend/src/routes/AppRouter.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { Suspense, lazy } from 'react';
 import Loading from '../components/common/Loading';
 
@@ -31,6 +31,7 @@ const AppRouter = () => {
 					</Route>
 					<Route path="/login" element={<Login />} />
 					<Route path="/signup" element={<Signup />} />
+					<Route path="*" element={<Navigate to="/" replace />} />
 				</Routes>
 			</Suspense>
 		</BrowserRouter>
