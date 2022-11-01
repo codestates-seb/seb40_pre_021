c64d0e760049e9313156b20c3526ed47aa280664
Seungyoon Yu <seung.yoon.yu@gmail.com>
2022-11-01 22:50:00 +0900

    FEAT: Question을 불러오는 API 추가

---
+++ frontend/src/api/QuestionApi.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const axiosConfig = {
+	baseURL: 'http://localhost:3001',
+};
+
+const instance = axios.create(axiosConfig);
+
+export const getQuestion = async (questionid) => {
+	try {
+		const result = await instance.get(`/questions/${questionid}`);
+		return result.data;
+	} catch (err) {
+		console.log(err);
+	}
+};
