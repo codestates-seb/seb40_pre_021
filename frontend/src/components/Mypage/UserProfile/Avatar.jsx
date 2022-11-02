344866bd40e43c7d9cb73b28dd13b0b4a557455f
jooyong-boo <qnwndyd159@naver.com>
2022-11-02 17:57:52 +0900

    Design: box-shadow에 props 추가

--- frontend/src/components/Mypage/UserProfile/Avatar.jsx
+++ frontend/src/components/Mypage/UserProfile/Avatar.jsx
@@ -20,8 +20,11 @@ const AvatarBox = styled.div`
 	justify-content: center;
 	align-items: center;
 	border-radius: 5px;
-	box-shadow: 0 1px 2px hsla(0, 0%, 0%, 0.05), 0 1px 4px hsla(0, 0%, 0%, 0.05),
-		0 2px 8px hsla(0, 0%, 0%, 0.05);
+	box-shadow: ${(props) =>
+		props.shadow
+			? `0 1px 2px hsla(0, 0%, 0%, 0.05), 0 1px 4px hsla(0, 0%, 0%, 0.05),
+		0 2px 8px hsla(0, 0%, 0%, 0.05)`
+			: ''};
 `;
 
 const AvatarText = styled.span`
