c3c3982472979a31ec15c8cf23a5d9826543f848
jooyong-boo <qnwndyd159@naver.com>
2022-11-02 17:54:34 +0900

    Feat: 클릭시 filter 및 scrollTop 기능 추가

--- frontend/src/components/Mypage/Activity/Sidebar.jsx
+++ frontend/src/components/Mypage/Activity/Sidebar.jsx
@@ -1,16 +1,6 @@
-import { useState } from 'react';
 import styled from 'styled-components';
 
-const Sidebar = () => {
-	const [tabs, setTabs] = useState(data);
-
-	const handleTabChange = (id) => {
-		let newTabs = tabs.map((tab) =>
-			tab.id === id ? { ...tab, clicked: true } : { ...tab, clicked: false },
-		);
-		setTabs(newTabs);
-	};
-
+const Sidebar = ({ tabs, onChange, onFilter, onScrollTop }) => {
 	return (
 		<Box>
 			{tabs.map((tab) => {
@@ -20,7 +10,9 @@ const Sidebar = () => {
 						key={id}
 						clicked={clicked}
 						onClick={() => {
-							handleTabChange(id);
+							onChange(id);
+							onFilter(id);
+							onScrollTop();
 						}}>
 						{name}
 					</Tab>
@@ -39,7 +31,7 @@ const Box = styled.div`
 	margin: 8px 0;
 	position: sticky;
 	top: 64px;
-	height: 500px;
+	height: 150px;
 `;
 
 const Tab = styled.a`
@@ -61,66 +53,3 @@ const Tab = styled.a`
 		background-color: ${(props) => (props.clicked ? '#E4E6E8' : '#e4e6e8')};
 	}
 `;
-
-let data = [
-	{
-		id: 0,
-		name: 'Summary',
-		clicked: true,
-	},
-	{
-		id: 1,
-		name: 'Answers',
-		clicked: false,
-	},
-	{
-		id: 2,
-		name: 'Questions',
-		clicked: false,
-	},
-	{
-		id: 3,
-		name: 'Tags',
-		clicked: false,
-	},
-	{
-		id: 4,
-		name: 'Reputation',
-		clicked: false,
-	},
-	// {
-	// 	id: 5,
-	// 	name: 'Badges',
-	// 	clicked: false,
-	// },
-	// {
-	// 	id: 6,
-	// 	name: 'Following',
-	// 	clicked: false,
-	// },
-	// {
-	// 	id: 7,
-	// 	name: 'Bounties',
-	// 	clicked: false,
-	// },
-	// {
-	// 	id: 8,
-	// 	name: 'Reputation',
-	// 	clicked: false,
-	// },
-	// {
-	// 	id: 9,
-	// 	name: 'All actions',
-	// 	clicked: false,
-	// },
-	// {
-	// 	id: 10,
-	// 	name: 'Responses',
-	// 	clicked: false,
-	// },
-	// {
-	// 	id: 11,
-	// 	name: 'Votes',
-	// 	clicked: false,
-	// },
-];
