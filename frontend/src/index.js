cf257c7c051e433748746d231b458bd638f77dd6
moon <moonjh9392@gmail.com>
2022-11-02 11:50:07 +0900

    Fix: redux 새로고침시 store 초기화 되는 현상 redux-persist를 이용하여 수정

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import store from './store/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
+import { PersistGate } from 'redux-persist/integration/react';
+import store, { persistor } from './store/store';
+import { Provider } from 'react-redux';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
@@ -12,7 +13,9 @@ const root = createRoot(container);
 root.render(
 	<React.StrictMode>
 		<Provider store={store}>
-			<App />
+			<PersistGate loading={null} persistor={persistor}>
+				<App />
+			</PersistGate>
 		</Provider>
 	</React.StrictMode>,
 );
--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,11 +1,43 @@
 import userReducer from '../modules/userReducer';
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import {
+	persistStore,
+	persistReducer,
+	FLUSH,
+	REHYDRATE,
+	PAUSE,
+	PERSIST,
+	PURGE,
+	REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import logger from 'redux-logger';
 
-// const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
-const store = configureStore({
-	reducer: {
-		user: userReducer,
-	},
+const persistConfig = {
+	key: 'root',
+	version: 1,
+	storage,
+};
+
+//reducer 합치는 부분
+const rootReducer = combineReducers({
+	//추가되는 reducer
+	user: userReducer,
 });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+	reducer: persistedReducer,
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware({
+			serializableCheck: {
+				ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+			},
+			// 		}).concat(logger), // logger: 테스트를 위해 쓰임 주석 풀면 console에 store 동작 찍힘
+			// });
+		}),
+});
+export const persistor = persistStore(store);
 
 export default store;
