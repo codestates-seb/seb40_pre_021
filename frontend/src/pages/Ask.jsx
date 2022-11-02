4a0272377ea94d8b204f7ae0f535c99bb0692270
Seungyoon Yu <seung.yoon.yu@gmail.com>
2022-11-02 21:41:22 +0900

    FIX: Form Action을 사용하지 않고 Axios를 사용하여 요청을 보내도록 수정

--- frontend/src/pages/Ask.jsx
+++ frontend/src/pages/Ask.jsx
@@ -17,7 +17,8 @@ const Ask = () => {
 			body: problem + expect,
 			tags: tags,
 		};
-		ask(data.stringify());
+		console.log(data);
+		ask(JSON.stringify(data));
 	};
 
 	const handleTitle = (e) => {
@@ -35,81 +36,80 @@ const Ask = () => {
 
 	return (
 		<Container>
-			<Formarea>
-				<AskQuestion>Ask a public question</AskQuestion>
-				<WritingTip>
-					<h2>Writing a good question</h2>
-					<p className="writingtip">
-						You’re ready to ask a programming-related question and this form
-						will help guide you through the process. <br />
-						Looking to ask a non-programming question? See{' '}
-						<a href="https://stackexchange.com/sites#technology">
-							the topics here
-						</a>{' '}
-						to find a relevant site.
-					</p>
-					<p className="steps">Steps</p>
-					<ul>
-						<li>Summarize your problem in a one-line title.</li>
-						<li>Describe your problem in more detail.</li>
-						<li>Describe what you tried and what you expected to happen.</li>
-						<li>
-							Add “tags” which help surface your question to members of the
-							community.
-						</li>
-						<li>Review your question and post it to the site.</li>
-					</ul>
-				</WritingTip>
-				<Group>
-					<SectionTitle>
-						<label htmlFor="title">Title</label>
-					</SectionTitle>
-					<Caption>
-						<label htmlFor="title">
-							Be specific and imagine you’re asking a question to another
-							person.
-						</label>
-					</Caption>
-					<Form
-						id="title"
-						name="title"
-						type="text"
-						maxlength="300"
-						placeholder="e.g. Is there an R function for finding the index of an element in a vector?"
-						onChange={handleTitle}
-					/>
-				</Group>
-				<Tip></Tip>
-				<Group>
-					<SectionTitle>What are the details of your problem?</SectionTitle>
-					<Caption>
-						Introduce the problem and expand on what you put in the title.
-						Minimum 20 characters.
-					</Caption>
-					<Editor callback={handleProblem} />
-				</Group>
-				<Tip></Tip>
-				<Group>
-					<SectionTitle>
-						What did you try and what were you expecting?
-					</SectionTitle>
-					<Caption>
-						Describe what you tried, what you expected to happen, and what
-						actually resulted. Minimum 20 characters.
-					</Caption>
-					<Editor callback={handleExpect} />
-				</Group>
-				<Tip></Tip>
-				<Group>
-					<SectionTitle>Tags</SectionTitle>
-					<Caption>
-						Add up to 5 tags to describe what your question is about. Start
-						typing to see suggestions.
-					</Caption>
-					<TagForm callback={handleTags} />
-				</Group>
-				<Button text="Review your question" callback={handleClick} />
-			</Formarea>
+			{/* <Formarea action="/question/ask" method="post"> */}
+			<AskQuestion>Ask a public question</AskQuestion>
+			<WritingTip>
+				<h2>Writing a good question</h2>
+				<p className="writingtip">
+					You’re ready to ask a programming-related question and this form will
+					help guide you through the process. <br />
+					Looking to ask a non-programming question? See{' '}
+					<a href="https://stackexchange.com/sites#technology">
+						the topics here
+					</a>{' '}
+					to find a relevant site.
+				</p>
+				<p className="steps">Steps</p>
+				<ul>
+					<li>Summarize your problem in a one-line title.</li>
+					<li>Describe your problem in more detail.</li>
+					<li>Describe what you tried and what you expected to happen.</li>
+					<li>
+						Add “tags” which help surface your question to members of the
+						community.
+					</li>
+					<li>Review your question and post it to the site.</li>
+				</ul>
+			</WritingTip>
+			<Group>
+				<SectionTitle>
+					<label htmlFor="title">Title</label>
+				</SectionTitle>
+				<Caption>
+					<label htmlFor="title">
+						Be specific and imagine you’re asking a question to another person.
+					</label>
+				</Caption>
+				<Form
+					id="title"
+					name="title"
+					type="text"
+					maxlength="300"
+					placeholder="e.g. Is there an R function for finding the index of an element in a vector?"
+					onChange={handleTitle}
+				/>
+			</Group>
+			<Tip></Tip>
+			<Group>
+				<SectionTitle>What are the details of your problem?</SectionTitle>
+				<Caption>
+					Introduce the problem and expand on what you put in the title. Minimum
+					20 characters.
+				</Caption>
+				<Editor callback={handleProblem} />
+			</Group>
+			<Tip></Tip>
+			<Group>
+				<SectionTitle>
+					What did you try and what were you expecting?
+				</SectionTitle>
+				<Caption>
+					Describe what you tried, what you expected to happen, and what
+					actually resulted. Minimum 20 characters.
+				</Caption>
+				<Editor callback={handleExpect} />
+			</Group>
+			<Tip></Tip>
+			<Group>
+				<SectionTitle>Tags</SectionTitle>
+				<Caption>
+					Add up to 5 tags to describe what your question is about. Start typing
+					to see suggestions.
+				</Caption>
+				<TagForm callback={handleTags} />
+			</Group>
+			<Button text="Review your question" callback={handleClick} />
+			{/* </Formarea> */}
 		</Container>
 	);
 };
