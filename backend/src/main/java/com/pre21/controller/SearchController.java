5c14199901fc29a3f60854961332a473e2c08b6a
mozzi327 <mozzi327@gmail.com>
2022-11-02 17:30:16 +0900

    Fix: 폴더 경로 수정

---
+++ backend/src/main/java/com/pre21/controller/SearchController.java
@@ -0,0 +1,13 @@
+package com.pre21.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class SearchController {
+
+
+    @GetMapping("/search/")
+}
--- backend/src/test/java/com/pre21/controller/util/config/WithAccountSecurityContextFactory.java
+++
@@ -1,2 +0,0 @@
-package com.pre21.controller.util.config;public class WithAccountSecurityContextFactory {
-}
