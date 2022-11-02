6186a828d21a1bc8e26a0a431db867ca0d1200b3
Seungyoon Yu <seung.yoon.yu@gmail.com>
2022-11-02 20:32:41 +0900

    FIX: 태그폼에서 e.target.value를 끌어낼 수 없어 라이브러리 사용을 취소하고 직접 구현하는 방향으로 선회

--- frontend/src/components/Ask/TagForm.jsx
+++ frontend/src/components/Ask/TagForm.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import { TagsInput } from 'react-tag-input-component';
 import styled from 'styled-components';
 const InputFormStyle = styled.div`
 	input {
@@ -16,7 +15,7 @@ const InputFormStyle = styled.div`
 		color: hsl(206, 100%, 40%);
 		font-size: 12px;
 	}
-	.rti--container {
+	.container {
 		input {
 			font-size: 12px;
 			font-weight: 500;
@@ -30,47 +29,70 @@ const InputFormStyle = styled.div`
 		border-radius: 2px;
 		padding: 0.3rem;
 	}
-	.rti--tag {
+	.tag {
 		display: inline;
 		padding: 0.25rem 0.2rem 0.25rem 0.2rem;
 		border-radius: 4px;
 		font-weight: 400;
 		color: hsl(206, 100%, 40%);
 	}
-	.rti--tag button {
+	.tag button {
 		padding: 0 0.2rem;
 		margin-left: 0.2rem;
 		color: hsl(206, 100%, 40%);
 		font-weight: 700;
 		border-radius: 3px;
 	}
-	.rti--tag button:hover {
+	.tag button:hover {
 		background-color: hsl(206, 100%, 40%);
 		color: var(--rti-tag);
 	}
-	.rti--container:focus-within {
+	.container:focus-within {
 		outline: none;
 		border-color: #9ecaed;
 		box-shadow: 0 0 10px #9ecaed;
 	}
 `;
 
-const TagForm = ({ separators, callback }) => {
-	const [selected, setSelected] = useState([]);
+const TagForm = () => {
+	const [tags, setTags] = useState([]);
+	function handleKeyDown(e) {
+		if (e.key !== 'Enter') return;
+		const value = e.target.value;
+		if (!value.trim()) return;
+		setTags([...tags, value]);
+		e.target.value = '';
+	}
+	function removeTag(i) {
+		setTags(tags.filter((el, idx) => idx !== i));
+	}
 	return (
 		<InputFormStyle>
-			<div>
-				<TagsInput
-					value={selected}
-					onKeyUp={setSelected}
-					onChange={callback}
-					name="tags"
-					placeHolder="e.g. (c# angularjs sql)"
-					separators={separators}
-				/>
+			<div className="container">
+				{tags.map((tag, idx) => (
+					<div className="tag" key={idx}>
+						<span>{tag}</span>
+						<button onClick={() => removeTag(idx)}>&times;</button>
+					</div>
+				))}
+				<input
+					type="text"
+					placeholder="e.g. (objective-c json windows)"
+					onKeyDown={handleKeyDown}></input>
 			</div>
 		</InputFormStyle>
 	);
 };
+// <InputFormStyle>
+// 	<div>
+// 		<TagsInput
+// 			value={selected}
+// 			onKeyUp={setSelected}
+// 			name="tags"
+// 			placeHolder="e.g. (c# angularjs sql)"
+// 			separators={separators}
+// 		/>
+// 	</div>
+// </InputFormStyle>
 
 export default TagForm;
