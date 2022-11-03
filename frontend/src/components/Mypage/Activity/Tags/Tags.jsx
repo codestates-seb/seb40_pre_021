bc8c10345237eb15abc773c2bed0b3467bd99558
jooyong-boo <qnwndyd159@naver.com>
2022-11-03 14:45:59 +0900

    Feat: Tag 탭 선택시 정렬버튼 및 조건부 렌더링 추가

--- frontend/src/components/Mypage/Activity/Tags/Tags.jsx
+++ frontend/src/components/Mypage/Activity/Tags/Tags.jsx
@@ -1,13 +1,51 @@
 import styled from 'styled-components';
 import ListBox from '../ListBox';
+import SortButtonGroup from '../SortButtonGroup';
 import Title from '../Title';
 import TagsList from './TagsList';
 
-const Tags = ({ tag, limit }) => {
+let sortData = [
+	{
+		id: 0,
+		name: 'Score',
+		clicked: true,
+	},
+	{
+		id: 1,
+		name: 'Name',
+		clicked: false,
+	},
+];
+
+const Tags = ({ tag, setTag, limit, handleTabChange, flex = false }) => {
+	const handleSortTags = (name) => {
+		let newTag = [];
+		switch (name) {
+			case 'Score':
+				newTag = [...tag].sort((a, b) => b.tagCount - a.tagCount);
+				break;
+			case 'Name':
+				newTag = [...tag].sort(function compare(a, b) {
+					if (a.title < b.title) return -1;
+					if (a.title > b.title) return 1;
+					return 0;
+				});
+				break;
+		}
+		setTag(newTag);
+	};
 	return (
 		<Container>
 			<TitleBox>
-				<Title title="Tags" number={tag?.length} flex={true} />
+				<Title
+					title="Tags"
+					number={tag?.length}
+					flex={flex}
+					handleTabChange={handleTabChange}
+				/>
+				{!flex ? (
+					<SortButtonGroup data={sortData} handleSortTags={handleSortTags} />
+				) : null}
 			</TitleBox>
 			<ListBox
 				text="You have not participated in any tags"
