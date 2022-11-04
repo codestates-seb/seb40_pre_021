7825ba32a003c817d2e7193ce06a26b655490a7e
jooyong-boo <qnwndyd159@naver.com>
2022-11-05 01:01:49 +0900

    Refactor: 사용하지 않는 import 정리

--- frontend/src/pages/Mypage/MypageSavesPage.jsx
+++ frontend/src/pages/Mypage/MypageSavesPage.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import styled from 'styled-components';
 import SavesLayout from '../../components/Mypage/Saves/SavesLayout';
 import Sidebar from '../../components/Mypage/Saves/Sidebar';
