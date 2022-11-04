5fcd507697fef760560220def348e7e801c73bd9
moon <moonjh9392@gmail.com>
2022-11-04 09:17:19 +0900

    Fix: Users 메뉴 로그인 일때만 보이기

--- frontend/src/components/LeftSideBar/LeftSideBar.jsx
+++ frontend/src/components/LeftSideBar/LeftSideBar.jsx
@@ -1,4 +1,7 @@
+import { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import styled from 'styled-components';
+import { selectIsLogin } from '../../modules/userReducer';
 import Menu from './Menu';
 
 const Container = styled.div`
@@ -26,6 +29,8 @@ const LeftMenuStyle = styled.div`
 `;
 
 const LeftMenu = () => {
+	const isLogin = useSelector(selectIsLogin);
+	console.log(isLogin);
 	const menus = [
 		{
 			id: 0,
@@ -39,16 +44,10 @@ const LeftMenu = () => {
 		},
 		{
 			id: 2,
-			name: 'Tags',
-			path: '',
-		},
-		{
-			id: 3,
 			name: 'Users',
 			path: '/users/activity',
 		},
 	];
-
 	const props = {};
 
 	return (
@@ -67,6 +66,9 @@ const LeftMenu = () => {
 								<div className="public">&nbsp;PUBLIC</div>
 							</div>
 						);
+					}
+					if (ele.name === 'Users') {
+						return isLogin && <Menu key={ele.id} {...props} />;
 					}
 					return <Menu key={ele.id} {...props} />;
 				})}
