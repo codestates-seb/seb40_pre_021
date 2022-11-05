6fd314c81e4281eea6bc70207cc097730db4049e
mozzi327 <mozzi327@gmail.com>
2022-11-05 18:25:23 +0900

    Fix: Cors 이슈 origin 허용 설정

---
+++ backend/src/main/java/com/pre21/security/filter/CORSFilter.java
@@ -0,0 +1,27 @@
+package com.pre21.security.filter;
+
+import org.springframework.stereotype.Component;
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class CORSFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        chain.doFilter(req, res);
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+}
