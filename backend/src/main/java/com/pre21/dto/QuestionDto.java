1f12efaf68e52383d5945ec632d7077d2a00c4e1
mozzi327 <mozzi327@gmail.com>
2022-11-06 23:38:59 +0900

    Fix: 수정중

--- backend/src/main/java/com/pre21/dto/QuestionDto.java
+++ backend/src/main/java/com/pre21/dto/QuestionDto.java
@@ -79,6 +79,7 @@ public class QuestionDto {
     @Getter
     @Setter
     @ToString
+    @NoArgsConstructor
     @AllArgsConstructor
     public static class CommentPost {
         private String comments;
