210a3bc00ec6c218170c7da761d5e61e91e93e5a
moon <moonjh9392@gmail.com>
2022-11-06 16:34:40 +0900

    fix: header content-type json 으로 변경

--- frontend/src/api/root.js
+++ frontend/src/api/root.js
@@ -4,11 +4,11 @@ import axios from 'axios';
 const root = process.env.REACT_APP_STACK_SERVER;
 
 const axiosConfig = {
+	headers: { 'Content-Type': 'application/json; charset=UTF-8' },
 	baseURL: root,
 };
 
 const instance = axios.create(axiosConfig);
-
 instance.defaults.withCredentials = true; // withCredentials 전역 설정
 
 export default instance;
